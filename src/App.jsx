8ea8d0acbade230c3d981aa5523de3962c60fcfb
ritu jha <ritu05491@gmail.com>
2025-08-25 15:20:20 +0530

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 // import { useState } from 'react' 
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
 
-import './App.css'
 
 function App() {
 
-
   return (
     <>
+     <Header />
+      <Outlet />
+      <Footer />
     </>
   )
 }
